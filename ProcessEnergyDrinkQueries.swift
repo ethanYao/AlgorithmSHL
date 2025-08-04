3915b26faf336183b2c924c88589707e3f0f7e8f
EthanYao <ethan007@yaozeyangs-Mac-mini.local>
2025-08-04 23:54:52 +0800

    modify process item

--- ProcessEnergyDrinkQueries.swift
+++ ProcessEnergyDrinkQueries.swift
@@ -1,47 +1,49 @@
-func processEnergyDrinkQueries(_ numOfCustomer: Int, _ numOfQueries: Int, _ queryDesc: Int, _ queries: [[Int]]) -> [Int] {
-    // 使用字典存储客户购买记录，key为客户ID，value为购买数量
-    var customerPurchases: [Int: Int] = [:]
-    var results: [Int] = []
+import Foundation
+
+func processQueries(_ numOfCustomer: Int, _ secondLine: String, _ queryLines: [String]) -> String {
+    // 解析第二行的两个整数
+    let secondLineComponents = secondLine.split(separator: " ").map { Int($0)! }
+    let numOfQueries = secondLineComponents[0]
+    let queryDesc = secondLineComponents[1]
     
-    for query in queries {
-        let queryType = query[0]
+    var customerQuantities = [Int: Int]()
+    var type2Results = [Int]()
+    
+    // 处理每一行查询
+    for queryLine in queryLines {
+        let queryComponents = queryLine.split(separator: " ").map { Int($0)! }
+        let queryType = queryComponents[0]
         
         if queryType == 1 {
-            // 类型1查询：记录/更新客户购买
-            let customerID = query[1]
-            let quantity = query[2]
-            
-            // 如果客户已存在，累加数量；否则创建新记录
-            customerPurchases[customerID, default: 0] += quantity
-            
+            let customerID = queryComponents[1]
+            let quantity = queryComponents[2]
+            customerQuantities[customerID, default: 0] += quantity
         } else if queryType == 2 {
-            // 类型2查询：查询范围内客户的总购买量
-            let startID = query[1]
-            let endID = query[2]
-            
+            let startID = queryComponents[1]
+            let endID = queryComponents[2]
             var totalQuantity = 0
             
-            // 遍历指定范围内的客户ID
-            for customerID in startID...endID {
-                totalQuantity += customerPurchases[customerID, default: 0]
+            for id in startID...endID {
+                totalQuantity += customerQuantities[id, default: 0]
             }
             
-            results.append(totalQuantity)
+            type2Results.append(totalQuantity)
         }
     }
     
-    return results
+    // 返回空格分隔的字符串，如果没有type2查询则返回空字符串
+    return type2Results.isEmpty ? "" : type2Results.map(String.init).joined(separator: " ")
 }
 
+
 // 测试示例
-let testQueries = [
-    [1, 3, 12],  // 客户3购买12罐
-    [2, 0, 2],   // 查询客户0-2的总购买量
-    [1, 1, 4],   // 客户1购买4罐
-    [1, 3, 2],   // 客户3再购买2罐
-    [2, 2, 4]    // 查询客户2-4的总购买量
+let testQueriesCopy = [
+    "1 3 12",  // 客户3购买12罐
+    "2 0 2",   // 查询客户0-2的总购买量
+    "1 1 4",   // 客户1购买4罐
+    "1 3 2",   // 客户3再购买2罐
+    "2 2 4"    // 查询客户2-4的总购买量
 ]
 
-let result = processEnergyDrinkQueries(4, 5, 3, testQueries)
-print(result.map { String($0) }.joined(separator: " "))
-// 输出应该是: 0 14
+let result1 = processQueries(4, "5 3", testQueriesCopy)
+print(result1)
