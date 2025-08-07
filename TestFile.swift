269021c621d0207895b873812baf94325d9a739e
EthanYao <yaozeyang_2014@163.com>
2025-08-07 18:50:55 +0800

    123

--- TestFile.swift
+++ TestFile.swift
@@ -1,34 +1,52 @@
 import Foundation
 
-func sortByFrequencyDescending() {
-//    let elementCount = Int(readLine()!)!
-//    let numberList = readLine()!.split(separator: " ").map { Int($0)! }
+func findShortestPathWithMagic() {
+    let totalCities = Int(readLine()!)!
+    let startCity = Int(readLine()!)!
+    let targetCity = Int(readLine()!)!
+    let magicSpellCount = Int(readLine()!)!
+    let totalRoads = Int(readLine()!.split(separator: " ")[0])!
     
-    let elementCount = 19
-    let testNum = "1 2 2 3 3 3 4 4 5 5 5 5 6 6 6 7 8 9 10"
-    let numberList = testNum.split(separator: " ").map { Int($0)! }
+    var cityGraph = Array(repeating: [(Int, Int)](), count: totalCities)
     
-    var frequencyMap = [Int: Int]()
-    var firstOccurrenceIndex = [Int: Int]()
-    
-    for (index, number) in numberList.enumerated() {
-        frequencyMap[number, default: 0] += 1
-        if firstOccurrenceIndex[number] == nil {
-            firstOccurrenceIndex[number] = index
-        }
+    for _ in 0..<totalRoads {
+        let roadData = readLine()!.split(separator: " ").map { Int($0)! }
+        cityGraph[roadData[0]].append((roadData[1], roadData[2]))
+        cityGraph[roadData[1]].append((roadData[0], roadData[2]))
     }
     
-    let sortedNumbers = numberList.sorted { firstNum, secondNum in
-        let firstFreq = frequencyMap[firstNum]!
-        let secondFreq = frequencyMap[secondNum]!
+    var distanceMatrix = Array(repeating: Array(repeating: Int.max, count: magicSpellCount + 1), count: totalCities)
+    distanceMatrix[startCity][0] = 0
+    
+    for _ in 0..<totalCities * (magicSpellCount + 1) {
+        var selectedCity = -1
+        var selectedSpells = -1
+        var minDistance = Int.max
         
-        if firstFreq != secondFreq {
-            return firstFreq > secondFreq
+        for city in 0..<totalCities {
+            for spells in 0...magicSpellCount {
+                if distanceMatrix[city][spells] < minDistance {
+                    minDistance = distanceMatrix[city][spells]
+                    selectedCity = city
+                    selectedSpells = spells
+                }
+            }
         }
-        return firstOccurrenceIndex[firstNum]! < firstOccurrenceIndex[secondNum]!
+        
+        guard selectedCity != -1 else { break }
+        
+        distanceMatrix[selectedCity][selectedSpells] = Int.max
+        
+        for (neighborCity, edgeWeight) in cityGraph[selectedCity] {
+            distanceMatrix[neighborCity][selectedSpells] = min(distanceMatrix[neighborCity][selectedSpells], minDistance + edgeWeight)
+            
+            if selectedSpells < magicSpellCount {
+                distanceMatrix[neighborCity][selectedSpells + 1] = min(distanceMatrix[neighborCity][selectedSpells + 1], minDistance)
+            }
+        }
     }
     
-    print(sortedNumbers.map { String($0) }.joined(separator: " "))
+    print(distanceMatrix[targetCity].min() == Int.max ? -1 : distanceMatrix[targetCity].min()!)
 }
 
-sortByFrequencyDescending()
+findShortestPathWithMagic()
